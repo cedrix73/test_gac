f5e1e196d7dc51466c37398d3deb7516bca74331
cedrix73 <felten.cedric73@gmail.com>
2021-06-22 14:37:43 +0200

    Generallayout and controls

--- classes/TicketRepository.php
+++ classes/TicketRepository.php
@@ -22,19 +22,17 @@ class TicketRepository {
     }
     
     /**
-     * @name        getDureeTotaleReelle
-     * @description Duree totale reelle des appels effectués après le 15/02/2012 inclus 
+     * @name        getTotalLenghtSentCall
+     * @description Duree totale reelle des appels emis après le 15/02/2012 inclus 
      * @return int  $results['duree_totale']
      */
-    public function getDureeTotaleReelle() {
+    public function getTotalLenghtSentCall() {
         $this->_sql = " SELECT SEC_TO_TIME(SUM(TIME_TO_SEC(appels_emis.duree_reel))) AS duree_totale " .
                       " FROM appels_emis " .
                       " INNER JOIN tickets ON appels_emis.id_ticket = tickets.id " .
                       " WHERE tickets.date_ticket >= '2012-02-15' ";
         $reponse = $this->dbaccess->execPreparedQuery($this->_sql);
-        $results=$this->dbaccess->fetchAssoc($reponse);
-        
-        return $results['duree_totale'];
+        return $reponse;
     }
     
 
@@ -47,32 +45,29 @@ class TicketRepository {
      * @return Array $results      
      */
     public function getTopDatasFactures(){
-        $this->_sql = " SELECT tickets.num_abonne, connexions.volume_facture " .
+        $this->_sql = " SELECT tickets.num_abonne AS num_abonne, connexions.volume_facture as volume_facture" .
                       " FROM connexions " .
                       "  INNER JOIN tickets ON connexions.id_ticket = tickets.id " .
                       " AND tickets.heure_ticket NOT BETWEEN '08:00:00' AND '18:00:00'  " .
                       " ORDER BY connexions.volume_facture DESC  " .
                       " LIMIT 0,10 ";
         $reponse = $this->dbaccess->execQuery($this->_sql);
-        $results=$this->dbaccess->fetchArray($reponse);
-        return $results;
+        return $reponse;
     }
 
 
     /**
-     * @name          quantiteTotaleSms
-     * @description   Retourne la quantité totale de SMS envoyés 
+     * @name          getAllSms
+     * @description   Retourne la quantité total    e de SMS envoyés 
      *                par l'ensemble des abonnés
      *  @return int  $results['nb_sms_tot']
      */
-    public function quantiteTotaleSms() {
+    public function getAllSms() {
         $this->_sql = " SELECT COUNT(id) AS nb_sms_tot " .
                       " FROM tickets  " .
                       " WHERE type_donnees LIKE 'envoi de sms%' ";
         $reponse = $this->dbaccess->execPreparedQuery($this->_sql);
-        $results=$this->dbaccess->fetchAssoc($reponse);
- 
-        return $results['nb_sms_tot'];
+        return $reponse;
     }
     
 
